be8f7e441b692dabf05f1bbba5fe481797d29235
royz <rajorshi.me@gmail.com>
2021-05-21 00:16:05 +0530

    add script to clean node modules and lock files

---
+++ clean.sh
@@ -0,0 +1,2 @@
+# clean all node modules and lock files
+ rm -rf node_modules/ package-lock.json pnpm-lock.yaml yarn.lock server/node_modules/ server/package-lock.json server/pnpm-lock.yaml server/yarn.lock
